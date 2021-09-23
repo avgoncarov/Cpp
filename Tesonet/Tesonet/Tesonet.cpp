eddb645228d38881883fa24c7dc22e72a1b819ae
Arthur <65226159+avgoncarov@users.noreply.github.com>
2021-09-23 12:03:29 +0300

    Add files via upload

---
+++ Tesonet/Tesonet/Tesonet.cpp
@@ -0,0 +1,34 @@
+// reading a text file
+#include <iostream>
+#include <fstream>
+#include <string>
+#define STEPS 128 //#of iterations
+#define GRIDSIZE 512.0 //size of canvas
+#define N 512
+#define MAXDIST 3
+
+int randomInteger(int n) 
+int cells[w][h]
+
+int main(void)()
+{
+    int i, j, k, ii, jj, dist, color;
+        //int cells[CELLS] = { 0 }; //cell contents at time t
+        int old[CELLS]; //cell contents at time t-1
+
+        cells[CELLS / 2] = 1; //init one cell to black
+
+        //simulate cellular automata
+        for (k = 1; k < STEPS; t++) {
+            //output current row in turtle graphics
+            for (i = 0; i < CELLS; i++) {
+                if (cells[i] == 1) {
+
+                }
+            }
+        }
+        return 0;
+}
+
+
+
